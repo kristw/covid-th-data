771844c1bf56b3cdf3e72246fe967cdc67ae8954
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-03-30 22:45:12 -0700

    fix: typo

--- scripts/cleanProvince.js
+++ scripts/cleanProvince.js
@@ -1,7 +1,7 @@
 const PROVINCE_MAP = {
   กรุงเทพ: 'กทม',
   นทบุรี: 'นนทบุรี',
-  นนทุบุรี: 'นนทุบุรี',
+  นนทุบุรี: 'นนทบุรี',
   ภูก็ต: 'ภูเก็ต',
   สมุุทรปราการ: 'สมุทรปราการ',
   ชลบุุรี: 'ชลบุรี',
@@ -14,4 +14,3 @@ module.exports = function cleanProvince(input) {
 
   return PROVINCE_MAP[province] || province;
 }
-
