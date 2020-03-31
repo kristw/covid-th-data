748229141671db1db9f6a1168bb62d271cf55372
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-03-31 09:56:35 -0700

    fix: add log

--- scripts/cleanProvince.js
+++ scripts/cleanProvince.js
@@ -9,6 +9,7 @@ const provinceNames = provinces
 const provinceNameSet = new Set(provinceNames);
 
 const PROVINCE_MAP = {
+  กรุงเทพมหานคร: 'กทม',
   กรุงเทพ: 'กทม',
   นทบุรี: 'นนทบุรี',
   นนทุบุรี: 'นนทบุรี',
@@ -17,16 +18,27 @@ const PROVINCE_MAP = {
   ชลบุุรี: 'ชลบุรี',
 };
 
+// Define cost functions.
+const insert = n => 1;
+const remove = n => 1;
+const update = (stringA, stringB) => stringA !== stringB ? 1 : 0;
+
 module.exports = function cleanProvince(input) {
   const province = input.trim();
-  if (province === '') return 'ไม่ทราบ';
+  if (province === '') {
+    return 'ไม่ทราบ';
+  }
 
   const manuallyCleaned = PROVINCE_MAP[province] || province;
   if (provinceNameSet.has(manuallyCleaned)) {
+    if (province !== manuallyCleaned) {
+      console.log(`Clean: "${province}" => "${manuallyCleaned}"`);
+    }
     return manuallyCleaned;
   }
 
   // Find closest match
-  const closestMatch = _.minBy(provinceNames, p => ed.levenshtein(manuallyCleaned, p));
+  const closestMatch = _.minBy(provinceNames, p => ed.levenshtein(manuallyCleaned, p, insert, remove, update).distance);
   console.log(`Clean: "${province}" => "${closestMatch}"`);
+  return closestMatch;
 }
